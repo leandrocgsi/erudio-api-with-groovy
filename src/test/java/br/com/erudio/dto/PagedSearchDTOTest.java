fc4d6fd0b410ad49e798d55ca91a86ea1b852a8b
Leandro Costa <leandrocgsi@gmail.com>
2016-02-12 10:49:10 -0200

    Change  test cases

--- src/test/java/br/com/erudio/dto/PagedSearchDTOTest.java
+++ src/test/java/br/com/erudio/dto/PagedSearchDTOTest.java
@@ -21,9 +21,14 @@ public class PagedSearchDTOTest {
 	}
 	
 	@Test
-	public void getBaseSelect() {
+	public void getBaseSelectTest() {
 		String baseSelect = "select p from Person p ";
 		assertEquals(dto.getBaseSelect("p", "Person"), baseSelect);
+	}
+	
+	@Test
+	public void getStartTest() {
+		assertEquals(dto.getStart(), (Integer)0);
 	}
 	
 	@Test
@@ -34,17 +39,35 @@ public class PagedSearchDTOTest {
 	
 	@Test
 	public void getWhereAndParametersTest() {
-		String whereClause = " where  p.phone = :phone and  p.name = :name and  p.email = :email and 1 = 1 ";
+		String whereClause = " where p.phone = :phone and p.name = :name and p.email = :email and 1 = 1 ";
+		assertEquals(whereClause, dto.getWhereAndParameters("p"));
+	}
+	
+	@Test
+	public void getWhereAndParametersWithBlankStringKeyTest() {
+		Map<String, Object> filters = mockFilters();
+		filters.put("", "LEANDRO");
+		dto.setFilters(filters);
+		String whereClause = " where p.phone = :phone and p.name = :name and p.email = :email and 1 = 1 ";
+		assertEquals(dto.getWhereAndParameters("p"), whereClause);
+	}
+
+	@Test
+	public void getWhereAndParametersWithBlankStringValueTest() {
+		Map<String, Object> filters = new HashMap<String, Object>();
+		filters.put("name", "");
+		dto.setFilters(filters);
+		String whereClause = " where 1 = 1 ";
 		assertEquals(dto.getWhereAndParameters("p"), whereClause);
 	}
 	
 	@Test
 	public void getHqlFindPersonTest() {
 		String selectWithParameters = "select p from Person p"
-				+ "  where  p.phone = :phone and"
-				+ "  p.name = :name and"
-				+ "  p.email = :email and 1 = 1 ";
-		assertEquals(dto.getSelectHql("p", "Person"), selectWithParameters);
+				+ "  where p.phone = :phone and"
+				+ " p.name = :name and"
+				+ " p.email = :email and 1 = 1 ";
+		assertEquals(selectWithParameters, dto.getSelectHql("p", "Person"));
 	}
 	
 	@Test
@@ -89,6 +112,9 @@ public class PagedSearchDTOTest {
 		filters.put("email", "a@b.c");
 		filters.put("phone", "12345678998");
 		filters.put("cpf", null);
+		filters.put("religion", null);
+		filters.put("cpf", null);
+		filters.put(null, "COSTA");
 		return filters;
 	}
 
