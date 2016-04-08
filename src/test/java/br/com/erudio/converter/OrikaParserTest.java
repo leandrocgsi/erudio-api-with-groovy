eede8614fe312bfe7582fd15f89c574eb2b1be76
Leandro da Costa Gonçalves <leandrocgsi@gmail.com>
2016-04-08 00:18:09 -0300

    Doing some more tests

--- src/test/java/br/com/erudio/converter/OrikaParserTest.java
+++ src/test/java/br/com/erudio/converter/OrikaParserTest.java
@@ -2,17 +2,21 @@ package br.com.erudio.converter;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
 
 import br.com.erudio.mocks.PersonMock;
+import br.com.erudio.mocks.PersonMockFromJSON;
 import br.com.erudio.vo.PersonVO;
 
 public class OrikaParserTest {
 
-    PersonMock inputObject;
+	PersonMock inputObject;
 
     @Before
     public void setUp() {
@@ -20,21 +24,48 @@ public class OrikaParserTest {
     }
 
     @Test
-    public void parseObjectInputToObjectOutputTest() {
+    public void parsePersonEntityToPersonVOTest() {
         PersonVO output = OrikaParser.parseObjectInputToObjectOutput(inputObject.mockPerson(1), PersonVO.class);
         System.out.println(output.getAddresses().get(0).getPostalCode());
         assertEquals("ONE PERSON 1", output.getName());
         assertEquals("34700-370", output.getAddresses().get(0).getPostalCode());
-//        assertTrue(output.getAge() == 21);
     }
 
     @Test
-    public void parserListObjectInputToObjectOutputTest() {
-        List<PersonVO> output = OrikaParser.parserListObjectInputToObjectOutput(inputObject.mockPersonsList(9), PersonVO.class);
-//        System.out.println(output.get(0).getAddresses().get(0).getPostalCode());
-        assertEquals("ONE PERSON 1", output.get(0).getName());
-//        assertEquals("Old City", output.get(0).getAddress().getTown());
-//        assertTrue(output.get(0).getAge() == 22);
+    public void parseListPersonsEntityToPersonsVOTest() {
+        List<PersonVO> persons = OrikaParser.parserListObjectInputToObjectOutput(inputObject.mockPersonsList(9), PersonVO.class);
+        assertEquals("ONE PERSON 1", persons.get(0).getName());
+        assertEquals("34700-370", persons.get(1).getAddresses().get(0).getPostalCode());
+        assertEquals("34700-371", persons.get(2).getAddresses().get(1).getPostalCode());
+        assertEquals("34700-372", persons.get(3).getAddresses().get(2).getPostalCode());
     }
+    
+    @Test
+	public void parsePersonEntityToPersonVOFromJSONTest(){
+        PersonVO output = OrikaParser.parseObjectInputToObjectOutput(mockPersonFromJSON(), PersonVO.class);
+        System.out.println(output.getAddresses().get(0).getPostalCode());
+        assertEquals("ONE PERSON 1", output.getName());
+        assertEquals("34700-370", output.getAddresses().get(0).getPostalCode());
+	}
 
+	public PersonVO mockPersonFromJSON(){
+		PersonVO person = new PersonVO();
+		try {
+			person = new ObjectMapper().readValue(PersonMockFromJSON.PERSON, PersonVO.class);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return person;
+	}
+	
+	public ArrayList<PersonVO> mockPersonsFromJSON(){
+//		ArrayList<PersonVO> dtoFromJSON = new ArrayList<>();
+//		try {
+//			dtoFromJSON = new ObjectMapper().readValue(PersonsMockFromJSON.PERSONS, PersonVO.class);
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+		return new ArrayList<>();
+	}
+    
 }
